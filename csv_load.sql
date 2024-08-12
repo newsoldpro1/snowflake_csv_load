c31adb9e72681d0a29d371af196592da839901c7
newsoldpro1 <newsoldpro@yahoo.com>
2024-08-12 17:16:48 -0600

    Update csv_load.sql

--- csv_load.sql
+++ csv_load.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE products;
-CREATE OR REPLACE mystage;
+CREATE DATABASE my_table;
+CREATE OR REPLACE my_stage;
 -- PUT FILE://C:/Users/kevin.seely/sample_products.csv @mycsvstage; --only in snowsql
-CREATE OR REPLACE FILE FORMAT myformat TYPE = csv, parse_header = true;
+CREATE OR REPLACE FILE FORMAT my_format TYPE = csv, parse_header = true;
 CREATE OR REPLACE TABLE products using template (
     SELECT array_agg(object_construct(*)) 
     FROM TABLE (infer_schema(
-        location => '@mystage',
+        location => '@my_stage',
         files => '.csv',
-        file_format => 'mycsvformat'))); 
+        file_format => 'my_format'))); 
 COPY INTO emp FROM @mycsvstage
     files = ('sample_products.csv')
-    file_format = (format_name=mycsvformat)
+    file_format = (format_name=my_format)
     match_by_column_name = case_sensitive;
